075c10e8231a4e0e2d7c4797cff9aa6e80c230b3
Your Name <you@example.com>
2016-02-04 15:56:17 -0500

    images and gitroot

--- stuff.mk
+++ stuff.mk
@@ -3,7 +3,7 @@ msrepo = https://github.com/dushoff
 courserepo = https://github.com/Bio3SS
 
 gitroot = ./gitroot
-Drop = ./gitroot/Drop
+Drop = $(gitroot)/Drop
 export ms = $(gitroot)/makestuff
 
 web = $(gitroot)/Bio3SS.github.io/materials/
@@ -14,8 +14,6 @@ web = $(gitroot)/Bio3SS.github.io/materials/
 export ms = $(gitroot)/makestuff
 images = $(Drop)/Lecture_images
 
-dirs = $(gitroot)/Population_time_series $(gitroot)/Exponential_figures $(gitroot)/Birth_death_models $(gitroot)/Compensation_models $(gitroot)/Life_tables $(gitroot)/Age_distributions $(gitroot)/Structured_models
-
 Makefile: $(ms) $(images) $(dirs)
 $(ms):
 	$(MAKE) -f stuff.mk $(gitroot)
@@ -25,10 +23,11 @@ $(dirs):
 	$(MAKE) -f stuff.mk $(gitroot)
 	cd $(gitroot) && git clone $(courserepo)/$(notdir $@).git
 
-$(gitroot):
+$(images): 
+	$(MAKE) -f stuff.mk $(gitroot)
+	-mkdir $(dir $(images))
 	mkdir $@
 
-$(images): 
-	-mkdir $(dir $(images))
+$(gitroot):
 	mkdir $@
 
