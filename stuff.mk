95d681db2105932212b92189fd3939b1778b84c3
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-02-03 23:57:24 -0500

    Absolute failure with testdir

--- stuff.mk
+++ stuff.mk
@@ -3,25 +3,32 @@ msrepo = https://github.com/dushoff
 courserepo = https://github.com/Bio3SS
 
 gitroot = ./gitroot
-Drop = ~/Dropbox/courses
+Drop = ./gitroot/Drop
 export ms = $(gitroot)/makestuff
 
 web = $(gitroot)/Bio3SS.github.io/materials/
 
 -include local.mk
 -include $(gitroot)/local.mk
+
 export ms = $(gitroot)/makestuff
 images = $(Drop)/Lecture_images
+
 dirs = $(gitroot)/Population_time_series $(gitroot)/Exponential_figures $(gitroot)/Birth_death_models $(gitroot)/Compensation_models $(gitroot)/Life_tables $(gitroot)/Age_distributions $(gitroot)/Structured_models
 
 Makefile: $(ms) $(images) $(dirs)
-$(ms): $(dir $(ms))
-	cd $(dir $(ms)) && git clone $(msrepo)/$(notdir $(ms)).git
-$(dir $(ms)):
+$(ms):
+	$(MAKE) -f stuff.mk $(gitroot)
+	cd $(gitroot) && git clone $(msrepo)/$(notdir $(ms)).git
+
+$(dirs): 
+	$(MAKE) -f stuff.mk $(gitroot)
+	cd $< && git clone $(courserepo)/$(notdir $@).git
+
+$(gitroot):
 	mkdir $@
 
-$(images): $(dir $(images))
-	mkdir $@
-$(dir $(images)):
+$(images): 
+	-mkdir $(dir $(images))
 	mkdir $@
 
